06cb52b4577872ca76c638a464a9af1d3508532a
Pascal <pascalmayr2@gmaill.com>
2020-04-19 05:58:48 +0200

    add admin user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except: [:show, :new, :create]
   before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcomme to the Alpha Blog #{@user.username}"
+      flash[:success] = "Welcomme to the Alpha Blog #{@user.username} please visit your inbox"
       redirect_to root_path
     else
       render 'new'
@@ -34,10 +35,10 @@ class UsersController < ApplicationController
     
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
-  def destroys
+  def destroy
     @user.destroy
-    redirect_to root_path
-    flash[:notice] = "User #{@user.username} was succesfully deleted"
+    redirect_to logout_path
+    flash[:notice] = "User #{@user.username} and all his artticles was succesfully deleted"
   end
   private
   def user_params
@@ -47,8 +48,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You have not the rights to access this users area"
+      redirect_to root_path
+    end
+  end
+  def require_admin
+    if current_user != @user and !current_user.admin? and logged_in?
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_113007) do
+ActiveRecord::Schema.define(version: 2020_04_15_143637) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_04_07_113007) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
+    t.boolean "emailconfirmation", default: false
   end
 
 end
